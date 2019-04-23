2a3b407ad5ea27c84621e53102488668968836ee
SerialForBreakfast <joe@showblender.com>
2019-04-23 13:47:14 -0700

    Built UITableView and Custom Cell

---
+++ BlueSleuth/BTTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  BTTableViewCell.swift
+//  BlueSleuth
+//
+//  Created by Joseph McCraw on 4/23/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import UIKit
+
+class BTTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    
+    @IBOutlet var RSSILabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BlueSleuth/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  BlueSleuth
+//
+//  Created by Joseph McCraw on 4/23/19.
+//  Copyright © 2019 Joseph McCraw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBAction func refreshTapped(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "BTCell", for: indexPath) as? BTTableViewCell {
+            cell.nameLabel.text = "This is a test"
+            cell.RSSILabel.text = "RSSI = 00000"
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
+
